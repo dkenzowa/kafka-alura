3214ce1dae44aa8a0405ed7540b34994544bf5f8
Danilo Kenzo <dani.kenzo@hotmail.com>
2022-11-21 10:48:23 -0300

    Adicionando generics para envio das mensagens do Kafka e adicionando Gson para serializar o generics

--- src/main/java/ecommerce/NewOrderMain.java
+++ src/main/java/ecommerce/NewOrderMain.java
@@ -1,21 +1,25 @@
 package ecommerce;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
 
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
-		try (var dispatcher = new KafkaDispatcher()) {
+		try (var orderDispatcher = new KafkaDispatcher<Order>()) {
+			try (var emailDispatcher = new KafkaDispatcher<String>()) {
+				for (var i = 0; i < 10; i++) {
+					var userId = UUID.randomUUID().toString();
+					var orderId = UUID.randomUUID().toString();
+					var amount = new BigDecimal(Math.random() * 5000 + 1);
+					var order = new Order(userId, orderId, amount);
 
-			for (var i = 0; i < 10; i++) {
-				var key = UUID.randomUUID().toString();
-				var value = key + ", 321, 5102";
+					orderDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
 
-				dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
-				var email = "Thank you for your order! We are processing your order!";
-
-				dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
+					var email = "Thank you for your order! We are processing your order!";
+					emailDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, email);
+				}
 			}
 		}
 	}
