6a9ad436a0527e300feb4a0b4ce269af78692490
Danilo Kenzo <dani.kenzo@hotmail.com>
2022-11-21 11:01:09 -0300

    Modificado a classe de log para aceitar gson e string

--- src/main/java/ecommerce/LogService.java
+++ src/main/java/ecommerce/LogService.java
@@ -3,46 +3,29 @@ package ecommerce;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
+import java.util.regex.Pattern;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
 public class LogService {
 	public static void main(String[] args) {
-		var consumer = new KafkaConsumer<String, String>(properties());
-		while (true) {
-			consumer.subscribe(Collections.singletonList("ECOMMERCE_SEND_EMAIL"));
-			var records = consumer.poll(Duration.ofMillis(100));
 
-			for (var record : records) {
-				if (!records.isEmpty()) {
-					System.out.println("Encontrei + " + records.count() + " registros!");
-					System.out.println("------------------------------------------");
-					System.out.println("Send email");
-					System.out.println(record.key());
-					System.out.println(record.value());
-					System.out.println(record.partition());
-					System.out.println(record.offset());
+		var logService = new LogService();
 
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-
-					System.out.println("Email sent");
-				}
-			}
+		try (var service = new KafkaService(LogService.class.getSimpleName(), Pattern.compile("ECOMMERCE.*"), logService::parse)) {
+			service.run();
 		}
 	}
-
-	private static Properties properties() {
-		var properties = new Properties();
-		properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-		properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-		properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-		properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, LogService.class.getSimpleName());
-		return properties;
+	
+	
+	private void parse(ConsumerRecord<String, String> record) {
+		System.out.println("--------------------------------------");
+		System.out.println("LOG: " + record.topic());
+		System.out.println(record.key());
+		System.out.println(record.value());
+		System.out.println(record.partition());
+		System.out.println(record.offset());
 	}
 }
