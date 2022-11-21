58a1f62bee9c70e44b92a8f1607b90d6ed19c18c
Danilo Kenzo <dani.kenzo@hotmail.com>
2022-11-21 09:23:29 -0300

    Melhorando o código, deixando mais simples

--- src/main/java/ecommerce/EmailService.java
+++ src/main/java/ecommerce/EmailService.java
@@ -1,41 +1,23 @@
 package ecommerce;
 
-import java.time.Duration;
-import java.util.Collections;
-import java.util.Properties;
-import java.util.regex.Pattern;
-
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 public class EmailService {
 	public static void main(String[] args) {
-		var consumer = new KafkaConsumer<String, String>(properties());
-		while (true) {
-			consumer.subscribe(Pattern.compile("ECOMMERCE.*"));
-			var records = consumer.poll(Duration.ofMillis(100));
+		var EmailService = new EmailService();
+		var service = new KafkaService(EmailService.class.getSimpleName(), "ECOMMERCE_SEND_EMAIL", EmailService::parse);
 
-			for (var record : records) {
-				if (!records.isEmpty()) {
-					System.out.println("Encontrei + " + records.count() + " registros!");
-					System.out.println("------------------------------------------");
-					System.out.println("LOG: " + record.topic());
-					System.out.println(record.key());
-					System.out.println(record.value());
-					System.out.println(record.partition());
-					System.out.println(record.offset());
-				}
-			}
-		}
+		service.run();
 	}
 
-	private static Properties properties() {
-		var properties = new Properties();
-		properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-		properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-		properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-		properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, LogService.class.getSimpleName());
-		return properties;
+	private void parse(ConsumerRecord<String, String> record) {
+		System.out.println("------------------------------------------");
+		System.out.println("Send email");
+		System.out.println("LOG: " + record.topic());
+		System.out.println(record.key());
+		System.out.println(record.value());
+		System.out.println(record.partition());
+		System.out.println(record.offset());
 	}
+
 }
